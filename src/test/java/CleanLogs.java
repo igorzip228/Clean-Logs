c6c1f77d75aa594531595333dd1c0dce6e443434
igorzip228 <igorzip228@gmail.com>
2018-05-28 16:59:01 +0300

    Logs 1

---
+++ src/test/java/CleanLogs.java
@@ -0,0 +1,29 @@
+import com.sun.javafx.scene.paint.MaterialHelper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CleanLogs {
+    public static void main(String[] args) {
+
+    }
+//функция принимает одну строку логов и очищает ее
+    private String trimActivity(String activity) {
+        StringBuilder cleanActivity = new StringBuilder();
+        cleanActivity.append(activity.substring(0, activity.indexOf("sky")));
+        cleanActivity.append("Activity:" );
+
+        String[] fieldNames = {"Login Username", "Data Object", "Recirds", "User Action", "User Action Status",
+                "Labels", "Service type", "Mapping Ids", "URI"};
+
+        Matcher matcher;
+        for (String field : fieldNames) {
+            matcher = Pattern.compile("\\[" + field + ":[^\\]]+(?:\\] )?");
+            if (matcher.find())
+                cleanActivity.append(matcher.group(0));
+        }
+
+        return cleanActivity.toString();
+    }
+
+}
